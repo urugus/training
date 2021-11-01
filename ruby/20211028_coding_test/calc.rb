bbb2a5861b135758f9ffeb647bab99cce87ae46c
suguru_sakashita <suguru@sugurusitanombp.lan>
2021-11-01 11:07:14 +0900

    [add]加減算を実行する関数と答えを出力するロジックを追加

--- ruby/20211028_coding_test/calc.rb
+++ ruby/20211028_coding_test/calc.rb
@@ -1,14 +1,7 @@
-# エラーコードと内容のリスト
-error_dict = {
-  'ValueError': 'first argument must be weight-value (ex: 1kg) (ArgumentError)',
-  'UnitError': 'the unit must be one of mg,g,kg (ArgumentError)',
-  'OperatorError': 'multiplier & divisor must be a number at "* , /"'
-}
-
 # 配列と単位を与えると最小単位に直した配列と最小単位を返す関数
 def change_min_unit(input_list, unit_dict, unit_list, min_sym)
   output_list = []
-  error_sym = ''
+  error_sym = nil
   input_list.each do |content|
     # 質量の場合、数値と単位に分解
     if mass = /(\d+)([a-z]+)/.match(content)
@@ -45,7 +38,7 @@ def multiply(input_list)
   unless oper_index.nil?
     product = input_list[oper_index + 1].to_i
     multiplicand = input_list[oper_index - 1]
-    # 被乗数を置換
+    # 被乗数を置換し、演算子と乗数を削除
     input_list[oper_index - 1] = multiplicand * product
     input_list.delete_at(oper_index)
     input_list.delete_at(oper_index)
@@ -62,7 +55,7 @@ def divide(input_list)
   unless oper_index.nil?
     product = input_list[oper_index + 1].to_i
     multiplicand = input_list[oper_index - 1]
-    # 被乗数を置換
+    # 被除数を置換し、演算子と除数を削除
     input_list[oper_index - 1] = multiplicand / product
     input_list.delete_at(oper_index)
     input_list.delete_at(oper_index)
@@ -73,11 +66,37 @@ def divide(input_list)
 end
 
 
+# 加減算を実行する関数
+def add_all(input_list)
+  list_len = input_list.length
+  input_list.insert(0, '+')
+  plus_sum = 0
+  minus_sum = 0
+  0.upto(list_len) {|i|
+  if input_list[i] == '+'
+    plus_sum += input_list[i+1]
+  elsif input_list[i] == '-'
+    minus_sum += input_list[i+1]
+  end
+  }
+  return plus_sum - minus_sum
+end
+
+
+
 # 実行関数
 def calc(input)
+
   # 単位の辞書
   unit_dict = {'kg': 1000000, 'g': 1000, 'mg': 1}
   unit_list = ['kg','g','mg']
+
+  # エラーコードと内容のリスト
+  error_dict = {
+    'ValueError': 'first argument must be weight-value (ex: 1kg) (ArgumentError)',
+    'UnitError': 'the unit must be one of mg,g,kg (ArgumentError)',
+    'OperatorError': 'multiplier & divisor must be a number at "* , /"'
+  }
 
   min_sym = :kg
   # 入力内容を半角スペースで分割して配列に格納
@@ -95,22 +114,27 @@ def calc(input)
   output_list = unit_changed_result[:list]
   min_sym = unit_changed_result[:min_sym]
   error_sym = unit_changed_result[:error_sym]
-  
-  puts output_list.to_s
+  # puts output_list.to_s
 
   # 乗除を実行
   output_list = multiply(output_list)
   output_list = divide(output_list)
+  # puts output_list.to_s
 
-    # end
   # 加減を実行
-  # 単位を付け直す
-    # 単位の辞書を参照し、記憶しておいた最小単位の値で割る
-    # 最小単位をつけて文字列に変換
-    # puts min_sym.to_s
-  puts output_list.to_s
-  puts error_sym
-  puts min_sym
+  ans_value = add_all(output_list)
+  # puts ans_value
+
+  # 単位の辞書を参照し、記憶しておいた最小単位の値で割る
+  # 最小単位をつけて文字列に変換
+  ans_weight = (ans_value.to_f / unit_dict[min_sym]).to_s + min_sym.to_s
+
+  # エラーがあればエラーを、なければ答えを出力
+  if error_sym.nil?
+    puts ans_weight
+  else
+    puts error_dict[error_sym]
+  end
 end
 
-calc('1kg * 4 + 3g * 3 / 2')
+calc('1kg * 4 - 3g * 3 / 2 + 2kg')
