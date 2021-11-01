1df77d4938d806b7c7dab8540e8c953b3be445c2
suguru_sakashita <suguru@sugurusitanombp.lan>
2021-11-01 11:28:21 +0900

    [add]異常系のテストを追加

--- ruby/20211028_coding_test/calc_test.rb
+++ ruby/20211028_coding_test/calc_test.rb
@@ -10,10 +10,16 @@ class CalcTest < Minitest::Test
         assert_equal '6100g', calc('100g + 2kg * 3')
         assert_equal '6025g', calc('100g / 4 + 2kg * 3')
         assert_equal '1042g', calc('1kg + 300g / 7')
+        assert_equal '4042g', calc('1kg * 2 * 2 + 300g / 7')
     end
 
     # 異常系のテスト実行
     def test_irregular
+        assert_equal "assert_equal '4042g', calc('1kg * 2 * 2 + 300g / 7')",
+            calc('+ 3g + 100mg')
+        assert_equal 'error: the unit must be one of mg,g,kg (ArgumentError)', '1kg + 3t'
+        assert_equal 'error: multiplier & divisor must be a number at "* , /"
+        (ArgumentError)', '1kg * 3g'
     end
 end
 
