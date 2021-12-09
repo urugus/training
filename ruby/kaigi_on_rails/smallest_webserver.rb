7496c83abbb36024e36a048828faa7d3beaa6624
s-sakashi <suguru.sakashita@gmail.com>
2021-12-09 17:14:23 +0900

    [change] 本物のサーバーをRubyで模倣

--- ruby/kaigi_on_rails/smallest_webserver.rb
+++ ruby/kaigi_on_rails/smallest_webserver.rb
@@ -3,19 +3,28 @@ require "socket"
 port = 3000
 server = TCPServer.open(port)
 
-# Wait for connection
-socket = server.accept
-
-
 loop do
-  line = socket.readline.chomp
-  puts "Read: #{line}"
+  # Wait for connection
+  socket = server.accept
+
+  puts "Got a connection!"
+  
+  if match = socket.gets.chomp.match(/^(?<verb>[A-Z]*) (?<path>[^ ]*) (?<ver>.*)$/)
+    headers = []
+    while line = socket.gets.chomp
+      break if line.bytesize == 0
+      headers << line.split(": ")
+    end
+
+    p VERB: match[:verb]
+    p PATH: match[:path]
+    p VERSION: match[:ver]
+    p HEADERS: headers
+  end
 
   if line.bytesize == 0
     socket.write "HTTP/1.1 200 OK\r\n"
-    socket.write "\r\n"
     socket.write "Hello World!\r\n"
     socket.close
-    break
   end
 end
